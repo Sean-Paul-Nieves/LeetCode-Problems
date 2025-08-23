7b777cd4b7f2ec0373ef922e550b5d5c92be8aec
Sean Paul Nieves <151922088+Sean-Paul-Nieves@users.noreply.github.com>
2025-08-23 20:54:10 +0800

    Time: 92 ms (6.30%) | Memory: 58.6 MB (91.16%) - LeetSync

--- 217-contains-duplicate/contains-duplicate.cs
+++ 217-contains-duplicate/contains-duplicate.cs
@@ -1,16 +1,10 @@
 public class Solution {
-    public bool ContainsDuplicate(int[] nums) {
-
-        Hashtable ht = new Hashtable();
-        int n = 0;
-        foreach (int num in nums) {
-            if (ht.ContainsKey(num)) {
-                return true;
-            }
-            else {
-                ht.Add(num, true);
-            }
-        }
-        return false;
+   public bool ContainsDuplicate(int[] nums) {
+    Array.Sort(nums); // O(n log n)
+    for (int i = 1; i < nums.Length; i++) {
+        if (nums[i] == nums[i - 1]) return true;
     }
+    return false;
+}
+
 }
