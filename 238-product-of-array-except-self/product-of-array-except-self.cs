a4ebad2c86ffd286109a48578cd152cf9d1f7e9b
Sean Paul Nieves <151922088+Sean-Paul-Nieves@users.noreply.github.com>
2025-08-28 11:09:54 +0800

    Time: 1 ms (100.00%) | Memory: 66.8 MB (67.28%) - LeetSync

--- 238-product-of-array-except-self/product-of-array-except-self.cs
+++ 238-product-of-array-except-self/product-of-array-except-self.cs
@@ -3,12 +3,11 @@ public class Solution {
         
         // Test Case to use is [1,2,3,4]
         int n = nums.Length;
-        int i;
         int[] answer = new int[n];
 
         // First Part: Prefix
         answer[0] = 1;
-        for (i = 1; i < n; i++) {
+        for (int i = 1; i < n; i++) {
             answer[i] = answer[i-1] * nums[i-1];
             /*
             answer[0] = 1 
@@ -24,7 +23,7 @@ public class Solution {
         // Second Part: Suffix
         // We are accessing the array backwards, so [6,2,1,1]
         int suffix = 1;
-        for (i = n - 1; i >= 0; i--) {
+        for (int i = n - 1; i >= 0; i--) {
             answer[i] *= suffix;
             suffix *= nums[i];
             /*
